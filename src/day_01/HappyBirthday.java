fbaa35cb00cd28bb0ec3836d29ea48cdd3240236
spingul <62077760+spingul@users.noreply.github.com>
2020-04-28 15:52:05 -0400

    Update HappyBirthday.java

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -5,6 +5,12 @@ public class HappyBirthday {
     public static void main(String[] args) {
 
         // Still need Practice!
+        
+        System.out.println("Happy birthday")
+                    System.out.println("Happy birthday")
+
+                    System.out.println("Happy birthday")
+
 
     }
 }
